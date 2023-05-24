ad7ec6a5679796aaa4923767251e30b872e155b5
ElectricS01 <electrics01@electrics01.com>
2023-05-24 03:57:54 -0400

    Updates

--- loops/loops/Form1.cs
+++ loops/loops/Form1.cs
@@ -10,6 +10,14 @@ using System.Windows.Forms;
 
 namespace loops
 {
+    /* Programmer name: Thomas
+     * 
+     * Task description: Implement a Windows Forms app that populates 
+     *                   a ListBox with numbers from 1 to the user-entered 
+     *                   value using a loop.
+     * 
+     * Completion date: Wed 24 May 2023
+     */
     public partial class loops : Form
     {
         public loops()
@@ -19,15 +27,23 @@ namespace loops
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // This event is triggered when the form is loaded
         }
 
         private void btnList_Click(object sender, EventArgs e)
         {
+            // Event handler for the button click event
+            // Clears the items in the ListBox
+            lstBox.Items.Clear();
+
+            // Tries to parse the value entered in the txtNum TextBox
             if (int.TryParse(txtNum.Text, out int value))
-                for (int i = 1; i <= int.Parse(txtNum.Text); i++)
             {
-                lstBox.Items.Add(i);
+                // Loops from 1 to the entered value and adds each number to the ListBox
+                for (int i = 1; i <= int.Parse(txtNum.Text); i++)
+                {
+                    lstBox.Items.Add(i);
+                }
             }
         }
     }
