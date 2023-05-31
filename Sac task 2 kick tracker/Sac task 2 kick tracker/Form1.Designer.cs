9fb2492ecc7398cefe28589cba1ff2a8a6ecf38f
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-05-31 14:16:39 +1000

    Updates

---
+++ Sac task 2 kick tracker/Sac task 2 kick tracker/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Sac_task_2_kick_tracker
+{
+    partial class kickTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_total = new System.Windows.Forms.Button();
+            this.btn_avg = new System.Windows.Forms.Button();
+            this.btn_top = new System.Windows.Forms.Button();
+            this.btn_rating = new System.Windows.Forms.Button();
+            this.txt_out = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(234, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kick Tracker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(31, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(429, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of kicks (1st quater):";
+            // 
+            // txt_1
+            // 
+            this.txt_1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_1.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_1.Location = new System.Drawing.Point(481, 115);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(178, 44);
+            this.txt_1.TabIndex = 2;
+            // 
+            // txt_2
+            // 
+            this.txt_2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_2.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_2.Location = new System.Drawing.Point(481, 169);
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(178, 44);
+            this.txt_2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(31, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(442, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number of kicks (2nd quater):";
+            // 
+            // txt_3
+            // 
+            this.txt_3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_3.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_3.Location = new System.Drawing.Point(481, 223);
+            this.txt_3.Name = "txt_3";
+            this.txt_3.Size = new System.Drawing.Size(178, 44);
+            this.txt_3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(31, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(435, 37);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Number of kicks (3rd quater):";
+            // 
+            // txt_4
+            // 
+            this.txt_4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txt_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_4.ForeColor = System.Drawing.SystemColors.Control;
+            this.txt_4.Location = new System.Drawing.Point(481, 281);
+            this.txt_4.Name = "txt_4";
+            this.txt_4.Size = new System.Drawing.Size(178, 44);
+            this.txt_4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(31, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(433, 37);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Number of kicks (4th quater):";
+            // 
+            // btn_total
+            // 
+            this.btn_total.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_total.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_total.Location = new System.Drawing.Point(38, 362);
+            this.btn_total.Name = "btn_total";
+            this.btn_total.Size = new System.Drawing.Size(314, 59);
+            this.btn_total.TabIndex = 9;
+            this.btn_total.Text = "Display Total Kicks";
+            this.btn_total.UseVisualStyleBackColor = false;
+            this.btn_total.Click += new System.EventHandler(this.btn_total_Click);
+            // 
+            // btn_avg
+            // 
+            this.btn_avg.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_avg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_avg.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_avg.Location = new System.Drawing.Point(369, 362);
+            this.btn_avg.Name = "btn_avg";
+            this.btn_avg.Size = new System.Drawing.Size(378, 102);
+            this.btn_avg.TabIndex = 10;
+            this.btn_avg.Text = "Display average number of kicks / quarter";
+            this.btn_avg.UseVisualStyleBackColor = false;
+            this.btn_avg.Click += new System.EventHandler(this.btn_avg_Click);
+            // 
+            // btn_top
+            // 
+            this.btn_top.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_top.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_top.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_top.Location = new System.Drawing.Point(38, 432);
+            this.btn_top.Name = "btn_top";
+            this.btn_top.Size = new System.Drawing.Size(314, 102);
+            this.btn_top.TabIndex = 11;
+            this.btn_top.Text = "Display quarter with the most kicks";
+            this.btn_top.UseVisualStyleBackColor = false;
+            this.btn_top.Click += new System.EventHandler(this.btn_top_Click);
+            // 
+            // btn_rating
+            // 
+            this.btn_rating.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_rating.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_rating.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_rating.Location = new System.Drawing.Point(369, 476);
+            this.btn_rating.Name = "btn_rating";
+            this.btn_rating.Size = new System.Drawing.Size(378, 58);
+            this.btn_rating.TabIndex = 12;
+            this.btn_rating.Text = "Display game rating";
+            this.btn_rating.UseVisualStyleBackColor = false;
+            this.btn_rating.Click += new System.EventHandler(this.btn_rating_Click);
+            // 
+            // txt_out
+            // 
+            this.txt_out.AutoSize = true;
+            this.txt_out.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txt_out.Location = new System.Drawing.Point(178, 551);
+            this.txt_out.Name = "txt_out";
+            this.txt_out.Size = new System.Drawing.Size(0, 37);
+            this.txt_out.TabIndex = 13;
+            this.txt_out.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // kickTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 619);
+            this.Controls.Add(this.txt_out);
+            this.Controls.Add(this.btn_rating);
+            this.Controls.Add(this.btn_top);
+            this.Controls.Add(this.btn_avg);
+            this.Controls.Add(this.btn_total);
+            this.Controls.Add(this.txt_4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "kickTracker";
+            this.Text = "Kick Tracking Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.TextBox txt_2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_total;
+        private System.Windows.Forms.Button btn_avg;
+        private System.Windows.Forms.Button btn_top;
+        private System.Windows.Forms.Button btn_rating;
+        private System.Windows.Forms.Label txt_out;
+    }
+}
+
---
+++ Sac task 2 kick tracker/Sac task 2 kick tracker/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sac_task_2_kick_tracker
+{
+    public partial class kickTracker : Form
+    {
+        public kickTracker()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_total_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txt_1.Text, out int k1) && int.TryParse(txt_2.Text, out int k2) && int.TryParse(txt_3.Text, out int k3) && int.TryParse(txt_4.Text, out int k4))
+            {
+                int res = k1 + k2 + k3 + k4;
+                txt_out.Text = "Total Kicks: " + res;
+            }
+
+        }
+
+        private void btn_avg_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txt_1.Text, out int k1) && int.TryParse(txt_2.Text, out int k2) && int.TryParse(txt_3.Text, out int k3) && int.TryParse(txt_4.Text, out int k4))
+            {
+                float res = (k1 + k2 + k3 + k4)/4f;
+                txt_out.Text = "Average Kicks: " + res;
+            }
+        }
+
+        private void btn_top_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txt_1.Text, out int k1) && int.TryParse(txt_2.Text, out int k2) && int.TryParse(txt_3.Text, out int k3) && int.TryParse(txt_4.Text, out int k4))
+            {
+                int[] arr = { k1, k2, k3, k4 };
+                int maxValue = arr.Max();
+                int res = arr.ToList().IndexOf(maxValue) + 1;
+                txt_out.Text = "Highest quater Kicks: " + res;
+            }
+        }
+
+        private void btn_rating_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txt_1.Text, out int k1) && int.TryParse(txt_2.Text, out int k2) && int.TryParse(txt_3.Text, out int k3) && int.TryParse(txt_4.Text, out int k4))
+            {
+                int total = k1 + k2 + k3 + k4;
+                string res = "Poor";
+                if (total >= 5 && total <= 8)
+                {
+                    res = "Average";
+                } 
+                else if (total >= 9 && total <= 12)
+                {
+                    res = "Good";
+                } 
+                else if (total >= 13)
+                {
+                    res = "Excellent";
+                }
+                txt_out.Text = "Game Rating: " + res;
+            }
+        }
+    }
+}
