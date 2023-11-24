05d34f65a9387b2ffb14032b3e9dd5dde918d50d
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-11-24 14:45:03 +1100

    Add Age in years

---
+++ Age in years/Age in years/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_in_years
+{
+    public partial class ageInYears : Form
+    {
+        public ageInYears()
+        {
+            InitializeComponent();
+        }
+
+        private void age_Click(object sender, EventArgs e)
+        {
+            int outVal = DateTime.Now.Year - born.Value.Year;
+            if (born.Value.AddYears(outVal) > DateTime.Now) outVal--;
+            if (outVal < 0)
+            {
+                ageOut.Text = "Invalid date";
+            } else
+            {
+                ageOut.Text = "You are: " + outVal.ToString() + " years old";
+            }
+        }
+    }
+}
